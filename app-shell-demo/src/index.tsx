0d064efdf6fd81cd6c756200d5a303b4191cdd5c
Satish Y <satishjs2297@gmail.com>
2021-04-18 10:25:22 +0530

    Add piral and pilet scafholding

---
+++ app-shell-demo/src/index.tsx
@@ -0,0 +1,18 @@
+import 'piral/polyfills';
+import { renderInstance } from 'piral';
+import { layout, errors } from './layout';
+import { createNgApi } from 'piral-ng';
+
+// change to your feed URL here (either using feed.piral.cloud or your own service)
+const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
+
+renderInstance({
+  layout,
+  errors,
+  plugins: [createNgApi()],
+  requestPilets() {
+    return fetch(feedUrl)
+      .then(res => res.json())
+      .then(res => res.items);
+  },
+});
---
+++ pilet-angular-demo/src/SampleTileComponent.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    template: `
+      <div class="tile">
+        <h4>Testing Application Angular</h4>
+      </div>
+    `,
+  })
+  export class SampleTileComponent {
+    constructor() {}
+  }
